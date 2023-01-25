10fac5d77cb5895dd6dad1e0488b6df1b7612472
Sondos-Hijab <sondos.hijab@gmail.com>
2023-01-25 17:02:18 +0200

    working on technologies section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiReact } from "react-icons/di";
+import { RxFigmaLogo } from "react-icons/Rx";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+  <Section id="tech">
+    <SectionDivider />
+    <br />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I have experience working with a variety of technologies within the web
+      development field. From front-end development to user experience and user
+      interface design.
+    </SectionText>
+
+    <List>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>Front-End</ListContainer>
+        <ListParagraph>
+          Experience with
+          <br /> ReactJS
+        </ListParagraph>
+      </ListItem>
+
+      <ListItem>
+        <RxFigmaLogo size="3rem" />
+        <ListContainer>UX/UI</ListContainer>
+        <ListParagraph>
+          Experience with <br />
+          Figma & Adobe XD
+        </ListParagraph>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
